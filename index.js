8e40ac6e675ae96a35e68c11fcf6b5e9ded1727e
anuj-thakur-513 <anujthakur2003.anuj@gmail.com>
2025-01-25 21:53:07 +0530

    add handler function

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const path = require("path");
+const ejs = require("ejs");
+const { Resend } = require("resend");
+
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
+
+exports.handler = async (event) => {
+  try {
+    const resendClient = new Resend(RESEND_API_KEY);
+
+    const data = JSON.parse(event.body);
+    const { receiverEmail, senderEmail, senderName } = data;
+    const filePath = path.join(__dirname, "templates/newEvent.ejs");
+    const html = await ejs.renderFile(filePath, {
+      name: senderName,
+      email: senderEmail,
+    });
+
+    const res = await resendClient.emails.send({
+      from: "Cally <noreply@password-manager.anuj-thakur.com>",
+      to: receiverEmail,
+      subject: "New Event Request",
+      html: html,
+    });
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+  }
+};
